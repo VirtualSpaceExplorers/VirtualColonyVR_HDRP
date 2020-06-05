398efd96c3b0d44814e68206de166d871a881af5
Roy Steenwijk <steenwijkhawk@gmail.com>
2020-06-05 03:36:08 +0200

    Most of the assetmanager client side is done

---
+++ unity-project/Assets/Src/EditorExtensions/FileTreeViewElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace Assets.Src.EditorExtensions
+{
+    public class FileTreeViewElement : TreeViewItem
+    {
+        public FileTreeViewElement(int id, int depth, string displayName) : base(id, depth, displayName)
+        {
+
+        }
+
+        public string RelativePath { get; set; }
+
+        public bool Synced { get; set; }
+
+        public bool IsFolder { get; set; }
+    }
+}
